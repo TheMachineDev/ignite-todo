0304205e9333198e3e1c63c2a538266f258fe8ef
TM <gustavoplm@hotmail.com>
2024-04-28 21:41:35 -0300

    Add functionality to toggle todos state

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,22 +1,29 @@
 import { CheckCircle, Trash } from '@phosphor-icons/react';
 import styles from './Todo.module.css';
 
-interface TodoProps {
+export interface TodoProps {
   id: string;
+  content: string;
+  isCompleted: boolean;
+  toggleTodoState: (id: string, isCompleted: boolean) => void;
 }
 
-export function Todo({ id }: TodoProps) {
+export function Todo({ id, content, isCompleted, toggleTodoState }: TodoProps) {
+
+  function handleTodoState(){
+    toggleTodoState(id, isCompleted);
+  }
 
   return (
     <div className={styles.todo}>
       <div className={styles.checkboxContainer}>
-        <input type="checkbox" id={id} />
+        <input type="checkbox" readOnly id={id} checked={isCompleted} onClick={handleTodoState}/>
         <label htmlFor={id}>
           <span><CheckCircle className={styles.checkCircle} weight="fill" size={21}/></span>
-          Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer lolololo.
+          {content}
         </label>
       </div>
-      <button title="Deletar tarefa">
+      <button title="Deletar tarefa" >
         <Trash size={18} weight="bold"/>
       </button>
     </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,25 +1,57 @@
-import { Todo } from './Todo';
+import { useState } from 'react';
+import { Todo, TodoProps } from './Todo';
 import styles from './TodoList.module.css';
 
+
+
 export function TodoList() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: '1',
+      content: 'Terminar o desafio',
+      isCompleted: false
+    },
+    {
+      id: '2',
+      content: 'Terminar o curso de React',
+      isCompleted: false
+    },
+    {
+      id: '3',
+      content: 'Terminar a primeira parte do modulo de React',
+      isCompleted: true
+    }
+  ]);
+
+  function toggleTodoState(todoToToggledState: string, isCompleted: boolean) {
+    const newTodosWithToggledOne = todos.map(todo => ({
+      ...todo,
+      ...(todo.id === todoToToggledState && {isCompleted: !isCompleted})
+    }))
+  
+    setTodos(newTodosWithToggledOne);
+  }
+  
+  //console.log(todos.filter(todo => todo.isCompleted === true).length)
+
   return (
     <>
       <header className={styles.listInfo}>
         <div className={styles.todoCounter}>
           <p>Tarefas criadas</p>
-          <span>0</span>
+          <span>{todos.length}</span>
         </div>
         <div className={styles.todoCounter}>
           <p>Concluídas</p>
-          <span>0</span>
+          <span>{todos.length > 0 ? `${todos.filter(todo => todo.isCompleted === true).length} de ${todos.length}` : 0}</span>
         </div>
       </header>
 
       <div className={styles.todoList}>
-        <Todo id="hello" />
-        <Todo id="hello1" />
-        <Todo id="hello2" />
-        <Todo id="hello3" />
+        {todos.map(todo => (
+          <Todo key={todo.id} id={todo.id} content={todo.content} isCompleted={todo.isCompleted} toggleTodoState={toggleTodoState}/>
+        ))}
       </div>
     </>
   )
