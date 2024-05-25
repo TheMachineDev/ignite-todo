92ebe7a0c67ee05e52dc21efbe03f832bc277be9
TM <gustavoplm@hotmail.com>
2024-05-25 17:42:42 -0300

    Checkbox css fixed to make it truly a circle and not an oval

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,35 +1,46 @@
-import { CheckCircle, Trash } from '@phosphor-icons/react';
-import styles from './Todo.module.css';
-import { TodoType } from '../App';
+import { CheckCircle, Trash } from "@phosphor-icons/react";
+import styles from "./Todo.module.css";
+import { TodoType } from "../App";
 
 interface TodoProps {
-  todo: TodoType
+  todo: TodoType;
   toggleTodoState: (id: string, isCompleted: boolean) => void;
   deleteTodo: (id: string) => void;
 }
 
 export function Todo({ todo, toggleTodoState, deleteTodo }: TodoProps) {
-
-  function handleTodoState(){
+  function handleTodoState() {
     toggleTodoState(todo.id, todo.isCompleted);
   }
 
-  function handleTodoDelete(){
+  function handleTodoDelete() {
     deleteTodo(todo.id);
   }
 
   return (
     <div className={styles.todo}>
       <div className={styles.checkboxContainer}>
-        <input type="checkbox" readOnly id={todo.id} checked={todo.isCompleted} onClick={handleTodoState}/>
+        <input
+          type="checkbox"
+          readOnly
+          id={todo.id}
+          checked={todo.isCompleted}
+          onClick={handleTodoState}
+        />
         <label htmlFor={todo.id}>
-          <span><CheckCircle className={styles.checkCircle} weight="regular" size={24}/></span>
-          {todo.content}
+          <span>
+            <CheckCircle
+              className={styles.checkCircle}
+              weight="regular"
+              size={24}
+            />
+          </span>
+          <p>{todo.content}</p>
         </label>
       </div>
       <button title="Deletar tarefa" onClick={handleTodoDelete}>
-        <Trash size={16} weight="bold"/>
+        <Trash size={16} weight="bold" />
       </button>
     </div>
-  )
+  );
 }
